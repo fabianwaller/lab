457579c17e5cd66e6c591a2b6c55aea4bfb1bc64
Maximilian Fickert <maximilian.fickert@gmail.com>
2020-02-09 09:55:48 +0100

    added simple lmcut example

---
+++ fai/examples/lmcut_simple.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+
+# Solve some tasks with A* and the LM-Cut heuristic.
+
+import fai.environments
+from fai.experiment import SimpleFDExperiment
+# the downward-benchmarks repository must be in your PYTHONPATH to import suites
+# see https://github.com/aibasel/downward-benchmarks
+import suites
+
+# adapt paths as necessary
+REPO = '/mnt/data_server/fickert/fast-downward'
+BENCHMARKS_DIR = '/mnt/data_server/fickert/downward-benchmarks'
+
+# runs the experiment on fai01-fai08, use get_fai1x_env() to use fai11-fai14 instead
+exp = SimpleFDExperiment(environment=fai.environments.get_fai0x_env())
+
+exp.add_suite(BENCHMARKS_DIR, suites.suite_optimal_strips())
+exp.add_algorithm('lmcut', REPO, 'master', ['--search', 'astar(lmcut())'])
+
+# Parse the commandline and show or run experiment steps.
+exp.run_steps()
